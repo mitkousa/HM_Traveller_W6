f00aacca17c849cc7d39fd27363b315497758303
mitkousa <uk.dimitar@gmail.com>
2021-08-25 22:57:35 +0100

    mvp done

---
+++ hw_traveller_start/models/traveller.js
@@ -0,0 +1,46 @@
+const Traveller = function(journeys) {
+  this.journeys = journeys;
+};
+
+Traveller.prototype.getJourneyStartLocations = function() {
+  return this.journeys.map((journey) => {
+    return journey.startLocation
+  })
+};
+
+Traveller.prototype.getJourneyEndLocations = function () {
+  return this.journeys.map((journey) => {
+    return journey.endLocation
+  })
+};
+
+Traveller.prototype.getJourneysByTransport = function (transport) {
+  return this.journeys.filter((journey) => {
+    return journey.transport === transport
+  })
+};
+
+Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
+  return this.journeys.filter((journey) => {
+    return journey.distance >= minDistance
+  },1000)
+};
+
+Traveller.prototype.calculateTotalDistanceTravelled = function () {
+  return this.journeys.reduce((total, journey) => {
+    return total += journey.distance
+  }, 0)
+};
+
+Traveller.prototype.getUniqueModesOfTransport = function () {
+  uniqueTransport = this.journeys.map((journey) => {
+   return journey.transport
+ })
+ return uniqueTransport.filter((journey, transport, journeys) => {
+   return journeys.indexOf(journey) === transport
+ })
+  
+};
+
+
+module.exports = Traveller;
